4eabbd47ab7f47bc214abcea2322aecd7d23839b
jinmyeong.heo <jinmyeong.heo@jinmyeongheoui-MacBookPro.local>
2023-07-12 23:05:08 +0900

    Initial commit

---
+++ src/test/java/jm/carrot/CarrotApplicationTests.java
@@ -0,0 +1,13 @@
+package jm.carrot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
