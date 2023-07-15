59fffd92afbd2d3fbe683bfed3f917c0de5b7f32
jinmyeong.heo <myng4291@naver.com>
2023-07-16 01:04:24 +0900

    BooleanToYNConverter

--- src/main/java/jm/carrot/entity/User.java
+++ src/main/java/jm/carrot/entity/User.java
@@ -3,8 +3,10 @@ package jm.carrot.entity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -16,9 +18,20 @@ public class User {
 
     private String mobileNumber;
 
+    /** 활성화 상태 */
+    private Boolean activated;
+
+    private String ratingScore;
+    @CreatedDate
+    private LocalDateTime createTime;
+
     @Builder
-    public User(Long id, String mobileNumber) {
+    public User(Long id, String mobileNumber, Boolean activated, String ratingScore, LocalDateTime createTime) {
         this.id = id;
         this.mobileNumber = mobileNumber;
+        this.activated = activated;
+        this.ratingScore = ratingScore;
+        this.createTime = createTime;
     }
+
 }
