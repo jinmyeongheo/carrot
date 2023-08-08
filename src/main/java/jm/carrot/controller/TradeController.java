1eb93bdc241cbf9edcd619ba72f741266c13298e
jinmyeong.heo <jinmyeong.heo@seeroo.co.kr>
2023-08-08 17:56:30 +0900

    수정

---
+++ src/main/java/jm/carrot/controller/TradeController.java
@@ -0,0 +1,11 @@
+package jm.carrot.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TradeController {
+
+    // 검색조건 selectBox
+    // 검색 리스트요약
+    // 검색 리스트
+}
